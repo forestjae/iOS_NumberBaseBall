88aa7d94bffb0b259faed136d89c638a906c879d
zoe <kny0651@naver.com>
2021-10-06 15:55:12 +0900

    refactor: 기회가 0이 될때까지 결과출력을 반복하는 함수 생성 및 코드 나누기

--- NumberBaseball/NumberBaseball/main.swift
+++ NumberBaseball/NumberBaseball/main.swift
@@ -6,67 +6,64 @@
 
 import Foundation
 
-// 변수 - 남은 시도횟수, (볼), 스트라이크 횟수, 임의의 정수 3개를 담을 변수
 
 var remainingAttempts: Int = 9
 
 var targetNumbers: [Int] = generateRandomNumbers()
 
-var playerNumbers: [Int]
-
-
-// 임의의 3개 정수 만들고 반환
-
 func generateRandomNumbers() -> [Int] {
-    var numbers: Set<Int> = []
-    while numbers.count < 3 {
+    var radomNumbers: Set<Int> = []
+    while radomNumbers.count < 3 {
         let number: Int = Int.random(in: 1...9)
-        numbers.insert(number)
+        radomNumbers.insert(number)
     }
-    return Array(numbers)
+    return Array(radomNumbers)
 }
 
-// 두 배열의 일치하는 숫자의 개수
-func countStrikeAndBallNumber(first: [Int], second: [Int]) -> Int {
-    let firstSet = Set<Int>(first)
-    let secondSet = Set<Int>(second)
+func countMatchingNumbers(of target: [Int], and playerNumbers: [Int]) -> Int {
+    let firstSet = Set<Int>(target)
+    let secondSet = Set<Int>(playerNumbers)
     let count = firstSet.intersection(secondSet).count
     return count
 }
 
-// 순서까지 일치하는 숫자의 개수
-func countStrike(first: [Int], second: [Int]) -> Int {
+func calculateStrikeCount(of target: [Int], and playerNumbers: [Int]) -> Int {
     var count = 0
     for index in 0...2 {
-        if first[index] == second[index] {
+        if target[index] == playerNumbers[index] {
             count += 1
         }
     }
     return count
 }
 
+print("정답 : \(targetNumbers[0]) \(targetNumbers[1]) \(targetNumbers[2])")
 
 
-
-while remainingAttempts > 0 {
-    var strikeCount: Int = 0
-    var ballCount: Int = 0
-    
-    playerNumbers = generateRandomNumbers()
-    print("정답 : \(targetNumbers[0]) \(targetNumbers[1]) \(targetNumbers[2])")
+func compareNumbers() {
+    let playerNumbers: [Int] = generateRandomNumbers()
     print("임의의 수 : \(playerNumbers[0]) \(playerNumbers[1]) \(playerNumbers[2])")
     
-    let totalCount = countStrikeAndBallNumber(first: targetNumbers, second: playerNumbers)
-    strikeCount = countStrike(first: targetNumbers, second: playerNumbers)
-    ballCount = totalCount - strikeCount
-    
+    let totalCount = countMatchingNumbers(of: targetNumbers, and: playerNumbers)
+    let strikeCount: Int = calculateStrikeCount(of: targetNumbers, and: playerNumbers)
+    let ballCount: Int = totalCount - strikeCount
+    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
     if strikeCount == 3 {
         print("사용자 승리!")
-        remainingAttempts = 1
+        remainingAttempts = 0
     }
-    
-    print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    remainingAttempts = remainingAttempts - 1
-    print("남은 기회 : \(remainingAttempts)\n")
 }
+
+func printResult() {
+    while remainingAttempts > 0 {
+        compareNumbers()
+        remainingAttempts -= 1
+        let result = remainingAttempts == 0 ? "컴퓨터 승리...!\n" : ""
+        print("남은 기회 : \(remainingAttempts)\n", terminator: result)
+    }
+}
+
+printResult()
+
+
 
